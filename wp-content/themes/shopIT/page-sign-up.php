ad7c8250341c6eda6d949c13d6f58d3e099b7f64
Nicholas-Kirui <nicholaskirui6@gmail.com>
2023-05-08 13:38:46 +0300

    Login and sign up pages

---
+++ wp-content/themes/shopIT/page-sign-up.php
@@ -0,0 +1,54 @@
+<?php get_header(); ?>
+<form action="">
+  <div class="s-up">
+    <div class="sign-up">
+      <div class="sheader1">
+        <h2>Sign Up</h2>
+      </div>
+      <div class="input1">
+        <label for="">Full Name</label>
+        <div class="icons1">
+          <ion-icon name="person-outline"></ion-icon>
+          <input type="string" placeholder="Enter full name">
+        </div>
+      </div>
+      <div class="input1">
+        <label for="">Email Address</label>
+        <div class="icons1">
+          <ion-icon name="mail-outline"></ion-icon>
+          <input type="string" placeholder="Enter email address">
+        </div>
+      </div>
+      <div class="input1">
+        <label for="">Phone Number</label>
+        <div class="icons1">
+          <ion-icon name="call-outline"></ion-icon>
+          <input type="string" placeholder="Enter phone number">
+        </div>
+      </div>
+      <div class="input1">
+        <label for="">Password</label>
+        <div class="icons1">
+          <ion-icon name="lock-open-outline"></ion-icon>
+          <input type="string" placeholder="Enter password">
+        </div>
+      </div>
+      <div class="input1">
+        <label for="">Re-type Password</label>
+        <div class="icons1">
+          <ion-icon name="lock-open-outline"></ion-icon>
+          <input type="string" placeholder="Re-type password">
+        </div>
+      </div>
+      <button class="button2">Continue</button>
+      <div class="lower1">
+        <p>Got an account?</p>
+        <a href="">Login</a>
+      </div>
+    </div>
+  </div>
+</form>
+<?php get_footer(); ?>
+
+
+<!-- testing -->
