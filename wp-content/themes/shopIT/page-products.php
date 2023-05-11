3a5e25020d6ba63e5a88a3cb40770f1474f4e20d
Patrick Mwaniki <patrickmwanikk@gmail.com>
2023-05-11 12:29:59 +0300

    add hover

--- wp-content/themes/shopIT/page-products.php
+++ wp-content/themes/shopIT/page-products.php
@@ -24,26 +24,32 @@ $books = $wpdb->get_results("SELECT * FROM $table");
     <div></div>
 </div>
 
+
 <div class="products-container">
     <div class="Cheader">
         <h2>Top picks</h2>
     </div>
     <hr>
+
     <div class="pcontainer">
         <?php
         foreach ($books as $book) {
         ?>
             <div class="item">
-                <div class="product">
-                    <img src="<?php echo $book->main_img; ?>" alt="">
-                    <p class="name"><?php echo $book->product_name; ?></p>
-                    <h2 class="price"><?php echo $book->price; ?></h2>
-                    <p class="discount"><?php echo $book->discount; ?></p>
-                </div>
+                <a href="">
+                    <div class="product">
+                        <img src="<?php echo $book->main_img; ?>" alt="">
+                        <p class="name"><?php echo $book->product_name; ?></p>
+                        <h2 class="price"><?php echo $book->price; ?></h2>
+                        <p class="discount"><?php echo $book->discount; ?></p>
+                    </div>
+                </a>
             </div>
         <?php } ?>
     </div>
+
 </div>
+
 
 
 <?php get_footer(); ?>
